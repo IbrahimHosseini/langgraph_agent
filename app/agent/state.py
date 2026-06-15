913fb8e25ac3e3f3105eacf769c5dcff03cd32f9
Ibrahim <sehosseini@me.com>
2026-06-15 06:59:24 +0330

    refactor: remove unused import of BaseMessage in agent state module

--- app/agent/state.py
+++ app/agent/state.py
@@ -1,7 +1,6 @@
 from typing import Annotated
 from typing_extensions import TypedDict
 import operator
-from langchain_core.messages import BaseMessage
 
 class AgentState(TypedDict):
     messages: Annotated[list, operator.add]
